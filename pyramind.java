0fd24bb2f650c3de5d78567bc9c74acf1dbe79ec
hfathim1 <68632970+hfathim1@users.noreply.github.com>
2020-07-24 15:19:21 +0530

    Update pyramind.java

--- pyramind.java
+++ pyramind.java
@@ -2,16 +2,8 @@ public class pyramind {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		int k =1;
-		for(int i=0;i<4;i++) {
-			for(int j=1;j<=4-i;j++) {
-				System.out.print(k);
-				System.out.print("\t");
-				k++;
-			}System.out.println();
-			
-		}
+System.out.println("Pyramind");
+		
 	}
 
 }
